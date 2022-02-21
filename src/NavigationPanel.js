9e8bc847459ece8b75940dc7bd696028afee187e
dydones <62013622+dydones@users.noreply.github.com>
2022-02-21 16:00:15 +0100

    Add files via upload

---
+++ src/NavigationPanel.js
@@ -0,0 +1,17 @@
+class NavigationPanel extends Component{
+	constructor(props){
+		super(props);
+		this.state={
+			page:"Navigation_panel",
+			isconnected:False
+		};
+	}
+		
+	render(){
+		return (
+			<div>
+				<h1> Ceci est ma putain de page de test </h1>
+			</div>
+		);
+	}
+}
